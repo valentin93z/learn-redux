bc022a354b096b479f7c2267f7bb66c7b08d856e
valentin93z <valentin.zagrebun.93@gmail.com>
2022-08-03 21:14:17 +0300

    3 in 1 app redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import './index.css';
+import { useDispatch } from 'react-redux';
+import { setFirstName, setLastName } from './features/user/userSlice';
+import { addTodo, toggleCompletedTodo, removeTodo } from './features/todo/todoSlice';
+import { getPosts, deletePostById } from './features/post/postSlice';
+import { useSelector } from 'react-redux';
+import { v4 } from 'uuid';
+
 
 function App() {
+
+  const dispatch = useDispatch();
+  const name = useSelector((state) => state.user.firstName);
+  const lastname = useSelector((state) => state.user.lastName);
+
+  const [todoValue, setTodoValue] = React.useState('');
+
+  const addTodoHandler = () => {
+    const todo = {
+      id: v4(),
+      text: todoValue,
+      completed: false,
+    }
+    dispatch(addTodo(todo));
+    setTodoValue('');
+  }
+
+  const todos = useSelector((state) => state.todo.todos);
+
+  const toggleTodoHandler = (id) => {
+    dispatch(toggleCompletedTodo(id));
+  }
+
+  const removeTodoHandler = (id) => {
+    dispatch(removeTodo(id));
+  }
+
+  const posts = useSelector((state) => state.post.posts);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <div className='block blockOne'>
+        <p>Enter your name:</p>
+        <input onChange={(e) => dispatch(setFirstName(e.target.value))} type="text" placeholder='First Name' />
+        <input onChange={(e) => dispatch(setLastName(e.target.value))} type="text" placeholder='Last Name' />
+        <div className='firstBlockOutput'>
+          <div>
+            <p>First Name:</p>
+            <p className='outName'>{name}</p>
+          </div>
+          <div>
+            <p>Last Name:</p>
+            <p className='outName'>{lastname}</p>
+          </div>
+        </div>
+      </div>
+      <div className='block blockTwo'>
+        <p>ToDo App</p>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input onChange={(e) => setTodoValue(e.target.value)} value={todoValue} type="text" placeholder='Type something...' />
+          <button onClick={() => addTodoHandler()} type="submit">Submit</button>
+        </form>
+
+        {
+          todos?.map((todo) => (
+            <div className='todoItem'>
+              <div className='completeItem' onClick={() => toggleTodoHandler(todo.id)}>Complete</div>
+              <div className={`textItem ${todo.completed ? 'todoCompleted' : ''}`}>{todo.text}</div>
+              <div className='deleteItem' onClick={() => removeTodoHandler(todo.id)}>Delete</div>
+            </div>
+          ))
+        }
+
+      </div>
+      <div className='block blockThree'>
+        <p>Async</p>
+        <button type="submit" onClick={() => dispatch(getPosts())}>Get posts</button>
+
+        {
+          posts?.map((post) => (
+            <div className='postItem' onClick={() => dispatch(deletePostById(post.id))}>
+              {post.title}
+            </div>
+          ))
+        }
+
+      </div>
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import userSlice from '../features/user/userSlice';
+import todoSlice from '../features/todo/todoSlice';
+import postSlice from '../features/post/postSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    user: userSlice,
+    todo: todoSlice,
+    post: postSlice,
   },
 });
